14303743096bca427b381c4f7156dad4f20b5222
Kapilan Satkunanathan <kasatkunanathan@gmail.com>
2022-05-17 23:03:14 -0400

    asset example added

---
+++ kycweb3site/assets/ConfettiButton.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Button } from '@nextui-org/react';
+import confetti from 'canvas-confetti';
+
+const ConfettiButton = () => {
+  const handleConfetti = () => {
+    console.log("HEllo");
+    confetti({});
+  };
+
+  return (
+    <Button
+      auto
+      rounded
+      ripple={false}
+      size="xl"
+      onClick={handleConfetti}
+      css={{
+        background: '$white',
+        fontWeight: '$semibold',
+        boxShadow: '$md',
+        position: 'relative',
+        overflow: 'visible',
+        color: '#0F9549',
+        px: '$18',
+        '&:after': {
+          content: '""',
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+          background: '$white',
+          opacity: 1,
+          borderRadius: '$pill',
+          transition: 'all 0.4s ease'
+        },
+        '&:hover': {
+          transform: 'translateY(-5px)',
+          '&:after': {
+            transform: 'scaleX(1.5) scaleY(1.6)',
+            opacity: 0
+          }
+        },
+        '&:active': {
+          transform: 'translateY(-2px)'
+        }
+      }}
+    >
+      Click me
+    </Button>
+  );
+};
+
+export default ConfettiButton;
