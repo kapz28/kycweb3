b2f44a76a5421df4030ef3d028a8eaf313dc7b22
Kapilan Satkunanathan <kasatkunanathan@gmail.com>
2022-05-25 23:34:06 -0400

    auth login a lot smoother

--- kycweb3site/pages/index.js
+++ kycweb3site/pages/index.js
@@ -5,16 +5,32 @@ import styles from '../styles/Home.module.css';
 import React, { useState } from 'react';
 import { Magic } from "magic-sdk";
 import { Button, Input, Spacer, Text } from "@nextui-org/react";
+import { useRouter } from 'next/router';
 
 export default function Home() {
   const [email, setEmail] = useState('');
   const [magic, setMagicLink] = useState('');
+  const router = useRouter();
   const loginwithMagicLink = async() => {
-    console.log(email);
-    console.log("PAPILAN");
-    var did = await new Magic('pk_live_67D8641A6E69A608');
-    setMagicLink(did);
-    did.auth.loginWithMagicLink({ email: email });
+    try {
+      var did = await new Magic('pk_live_67D8641A6E69A608');
+      setMagicLink(did);
+      await did.auth.loginWithMagicLink({ email: email });
+      if(did.auth){
+        router.push('/about');
+      }else{
+        const isLoggedIn = await magic.user.isLoggedIn();
+        if (isLoggedIn) {
+            router.push('/about');
+        }else{
+          console.log("not Loggged in");
+        }
+      }
+    }
+    catch (e) {
+      console.log(e);
+      console.log("Something went wrong, Try Again");
+    }
   };
 
   return (
