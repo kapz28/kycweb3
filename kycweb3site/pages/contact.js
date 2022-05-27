afe4ff464ac537122a28fba7034e943bcabbc359
Kapilan Satkunanathan <kasatkunanathan@gmail.com>
2022-05-26 23:20:34 -0400

    verify twitter

--- kycweb3site/pages/contact.js
+++ kycweb3site/pages/contact.js
@@ -5,11 +5,13 @@ import styles from '../styles/Home.module.css'
 import { Button, Input, Spacer, Text } from "@nextui-org/react";
 import { GraphQLClient, gql } from "graphql-request";
 import CyberConnect, { Env, Blockchain } from "@cyberlab/cyberconnect";
+import { twitterAuthorize, twitterVerify } from "@cyberlab/social-verifier";
 
 
 
 export default function Contact(){
     const [cyberConnection, setCyberConnection] = useState('');
+    const [handle, setHandle] = useState("");
     useEffect(() => {
         const cyberConnect = new CyberConnect({
             namespace: "CyberConnect",
@@ -26,7 +28,7 @@ export default function Contact(){
         return Boolean(ethereum && ethereum.isMetaMask);
     };
     
-    const handleOnClick = async () => {
+    const handleMetaMaskConnectClick = async () => {
         if (isMetaMaskInstalled()) {
             // Request to connect to MetaMask
             try {
@@ -57,17 +59,68 @@ export default function Contact(){
         }
     };
 
+    const handleTwitterClick = async () => {
+        // Get the MetaMask wallet address
+        const accounts = await window.ethereum.request({
+            method: "eth_requestAccounts"
+        });
+  
+        // Check clause for handle
+        if (!handle) return;
+    
+        // Verify the Twitter account
+        try {
+            await twitterVerify(accounts[0], handle);
+            alert(`Success: you've verified ${handle}!`);
+        } catch (error) {
+            console.error(error.message);
+        }
+    };
+
+    const handleTweetTwitterClick = async () => {
+        // Get the MetaMask wallet address
+        const accounts = await window.ethereum.request({
+            method: "eth_requestAccounts"
+        });
+  
+        // Prompt to enter the Twitter handle
+        const handle = prompt("Enter your Twitter handle:");
+    
+        // Check clause for handle
+        if (!handle) return;
+    
+        // Update the state for handle
+        setHandle(handle);
+    
+        // Generate the signature
+        const sig = await twitterAuthorize(window.ethereum, accounts[0], handle);
+    
+        // The message that the user posts on Twitter
+        const message = `Verifying my Web3 identity on @cyberconnecthq: %23LetsCyberConnect %0A ${sig}`;
+    
+        // Open new window so that the user can post on Twitter
+        window.open(`https://twitter.com/intent/tweet?text=${message}`, "_blank");
+    };
+
     return(
         <div className={styles.container}>
             <main className={styles.main}>
                 <Text h1>Cyber Connect</Text>
-                <Spacer y={2.5} />
-                <Button auto color="gradient" rounded bordered onPress={() => handleOnClick()}>
+                <Spacer y={1.5} />
+                <Button auto color="gradient" rounded bordered onPress={() => handleMetaMaskConnectClick()}>
                     Connect
                 </Button>
-                <Spacer y={2.5} />
+                <Spacer y={1.5} />
                 <Button auto color="gradient" rounded bordered onPress={() => handleFollowClick()}>
                     Follow
+                </Button>
+                <Spacer y={1.5} />
+                <Button auto color="gradient" rounded bordered onPress={() => handleTwitterClick()}>
+                    Verify Twitter
+                </Button>
+                <Spacer y={1.5} />
+                <Button auto color="gradient" rounded bordered onPress={() => handleTweetTwitterClick()}>
+                    Tweet Twitter
                 </Button>
             </main>
         </div>
